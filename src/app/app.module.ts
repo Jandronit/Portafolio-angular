9c76ab4afc882d2ce9f6713f2eec4d74245cc925
Jandronit <alejandro.arguello@bosonit.com>
2021-05-27 17:57:27 +0200

    Fin seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +17,10 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
     HeaderComponent,
     FooterComponent,
     PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
